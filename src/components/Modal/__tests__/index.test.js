54c255c3e29c0d58d35edd199c35b532146460b9
Matthew Parker <mattparker124@gmail.com>
2022-07-07 02:50:04 -0500

    added modal tests

---
+++ src/components/Modal/__tests__/index.test.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Modal from '../'
+
+const currentPhoto = {
+    name: 'Park bench',
+    category: 'landscape',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
+    index: 1
+};
+
+afterEach(cleanup)
+
+describe('Modal component', () => {
+    it('renders', () => {
+        render(<Modal
+            currentPhoto={currentPhoto}
+        />)
+    })
+
+    it('matches snapshot', () => {
+        const { asFragment } = render(<Modal
+            currentPhoto={currentPhoto}
+        />)
+        expect(asFragment()).toMatchSnapshot();
+    });
+});
