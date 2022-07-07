d51be3cbd4f9f9bdadf52d21388c809c8d8de493
Matthew Parker <mattparker124@gmail.com>
2022-07-07 01:27:15 -0500

    add validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value });
+        }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
     }
 
     return (
